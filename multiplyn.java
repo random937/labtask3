6ed668f9dac48188900c5385d51cd689147c7cfc
random937 <hubdomain2@gmail.com>
2025-11-24 11:23:47 +0600

    Add files via upload

---
+++ multiplyn.java
@@ -0,0 +1,6 @@
+public class multiplyn {
+  public static void main(String[] args) {
+    System.out.println(2 * 5);
+  }
+}
+
---
+++ string.java
@@ -0,0 +1,7 @@
+public class string {
+  public static void main(String[] args) {
+    String name = "John";
+    System.out.println(name);
+  }
+}
+
